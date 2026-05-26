04f343b2c5f91db0ae7bfe3da776e54638b77471
nebHailemariam <nebhailema@gmail.com>
2026-05-25 22:02:19 -0400

    feat: add navigation and footer components, and update global styles

--- my-website/app/layout.tsx
+++ my-website/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
+import Navigation from "@/components/navigation/Navigation";
+import Footer from "@/components/footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body className="bodyContainer">
+        <Navigation />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
